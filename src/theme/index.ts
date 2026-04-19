137b84ac94a36ec199d7563a5cd15a3619578870
az-badri <badrtdinoff.az27@gmail.com>
2026-04-19 19:40:50 +0300

    chore: theme tokens styles

---
+++ src/theme/index.ts
@@ -0,0 +1,11 @@
+export {
+  darkTheme,
+  lightTheme,
+  palette,
+  radii,
+  spacing,
+  typography,
+} from './tokens';
+export type { Theme, ThemeColors } from './tokens';
+
+export { useAppTheme } from './useAppTheme';
